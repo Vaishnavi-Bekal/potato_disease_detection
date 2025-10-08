044c4bbafd8bf5884b9b4e3b0d1628b71e7dc1da
Vaishnavi bekal <bvaishnavi131@gmail.com>
2025-10-08 21:52:53 +0530

    potato_disease_detection Initial commit

--- src/api/app.py
+++ src/api/app.py
@@ -12,8 +12,8 @@ model.load_state_dict(torch.load('best.pth', map_location=device))
 model.to(device).eval()
 
 tfm = transforms.Compose([
-    transforms.Resize(299),
-    transforms.CenterCrop(299),
+    transforms.Resize(224),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225])
 ])
